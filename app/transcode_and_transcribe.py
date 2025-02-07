8b112c6f885cc8825fc678e7077fa40edff6da08
Zach Walker <walkcodes@gmail.com>
2025-02-06 18:28:45 -0600

    Rendition 2: Testing quality vs bandwidth UHD 12M bitrate

--- app/transcode_and_transcribe.py
+++ app/transcode_and_transcribe.py
@@ -300,8 +300,8 @@ def process_video(s3_bucket, input_path, video_table, uhd_enabled, include_downl
                     "name": "UHD-H264",
                     "size": "3840x2160",
                     "video_codec": "h264_nvenc",
-                    "video_opts": "-preset slow -rc vbr_hq -b:v 8M -cq:v 20 -qmin 19 -qmax 26 -maxrate 8M -bufsize 16M -profile:v main",
-                    "bitrate": "8M",  # Estimated bandwidth
+                    "video_opts": "-preset slow -rc vbr_hq -b:v 12M -cq:v 20 -qmin 19 -qmax 26 -maxrate 15M -bufsize 30M -profile:v main",
+                    "bitrate": "12M",  # Estimated bandwidth
                     "codec": "avc1.640028",
                     "audio_opts": "-c:a aac -b:a 128k"
                 },
@@ -309,7 +309,7 @@ def process_video(s3_bucket, input_path, video_table, uhd_enabled, include_downl
                     "name": "1080P-H264",
                     "size": "1920x1080",
                     "video_codec": "h264_nvenc",
-                    "video_opts": "-preset slow -rc vbr_hq -b:v 5M -cq:v 21 -qmin 19 -qmax 28 -maxrate 5M -bufsize 10M -profile:v main",
+                    "video_opts": "-preset slow -rc vbr_hq -b:v 5M -cq:v 21 -qmin 19 -qmax 28 -maxrate 6M -bufsize 12M -profile:v main",
                     "bitrate": "5M",  # Estimated bandwidth
                     "codec": "avc1.640028",
                     "audio_opts": "-c:a aac -b:a 128k"
@@ -318,7 +318,7 @@ def process_video(s3_bucket, input_path, video_table, uhd_enabled, include_downl
                     "name": "720P-H264",
                     "size": "1280x720",
                     "video_codec": "h264_nvenc",
-                    "video_opts": "-preset slow -rc vbr_hq -b:v 3M -cq:v 22 -qmin 20 -qmax 30 -maxrate 3M -bufsize 6M -profile:v main",
+                    "video_opts": "-preset slow -rc vbr_hq -b:v 3M -cq:v 22 -qmin 20 -qmax 30 -maxrate 4M -bufsize 8M -profile:v main",
                     "bitrate": "3M",  # Estimated bandwidth
                     "codec": "avc1.64001f",
                     "audio_opts": "-c:a aac -b:a 128k"
